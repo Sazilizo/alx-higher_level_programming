cfe2eb9c3e67543125025261594d11ee9f74ee26
Sazilizo <lizosazi@gmail.com>
2023-08-28 12:28:38 -0700

    2-safe_print_in

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+        except IndexError:
+            return (0)
+    print("")
+    return (count)
