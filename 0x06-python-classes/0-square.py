a7c48b747d498a715a2f920462371ab26515f869
Sazilizo <lizosazi@gmail.com>
2023-08-29 14:07:05 -0700

    2-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines a class named Square"""
 
+
 class Square:
     """empty class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+"""creates a square"""
+
 
 class Square:
+    """an empty square class"""
     def __init__(self, size):
+        """initializes asquare class"""
+        """
+        Args:
+            size (int): The size of the new square."""
         self.__size = size
