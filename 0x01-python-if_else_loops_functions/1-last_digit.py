af17db8d2fc8d421d7c383e115bd46612745c3ac
Sazilizo <lizosazi@gmail.com>
2023-08-30 04:25:43 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+last = abs(number) % 10
 
-if last_digit > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
-elif last_digit == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
+if number < 0:
+    last = -last
+print("Last digit of {} is {} and is ".format(number, last), end="")
+
+if last > 5:
+    print("greater than 5")
+elif last == 0:
+    print("0")
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(0, 10):
-    for n in range(i + 1, 10):
-        if i == 8 and n == 9:
-             print("{}{}".format(i, 2))
+for num1 in range(0, 10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
         else:
-            print("{}{}".format(i, n), end=", ")
+            print("{}{}".format(num1, num2), end=", ")
