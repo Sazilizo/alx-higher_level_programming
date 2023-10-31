e72f1ebed1e7bc92502bb03a2f1f47c8e9e32380
Sazilizo <lizosazi@gmail.com>
2023-10-31 03:49:26 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 
+
 """
 a module consisting of a Rectangle class
 that has a getter and setter for both width and height
 """
 
-class Rectangle:
 
+class Rectangle:
+    """A Rectangle class with attributes  width and height"""
     def __init__(self, width=0, height=0):
         """
-            Initialize a Rectangle instance with a given width and height.
+       Initialize a Rectangle instance with a given width and height.
 
-            :param width: The width of the rectangle.
-            :param height: The height of the rectangle.
+        :param width: The width of the rectangle.
+        :param height: The height of the rectangle.
         """
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
