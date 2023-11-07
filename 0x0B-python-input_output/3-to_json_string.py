05ffb3f91f380579781d083b0cd22c0a22707644
Sazilizo <lizosazi@gmail.com>
2023-11-07 11:59:38 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import json
+""" A module that encodes a python string to json"""
+
+
+def to_json_string(my_obj):
+    """
+    Encodes the string passed to json data
+
+    Args:
+        my_obj: (any) object to encode
+
+    return:
+        encoded json string
+    """
+
+    to_json = json.dumps(my_obj)
+    return (to_json)
