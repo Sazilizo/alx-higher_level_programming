5ed8b6a65bc99d398878ab11dbbd49b27f327225
Sazilizo <lizosazi@gmail.com>
2023-11-13 11:04:17 -0800

    documentation to methods rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,6 +17,7 @@ class Rectangle(Base):
         Base: (class) Parent class
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Inittializing all the attributes"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -25,32 +26,59 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """
+        Getter function for height
+        returns the private height attribute
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        This methos sets the private height attribute to value
+        """
         self.__height = value
 
     @property
     def width(self):
+        """Return the value of private attrbute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the value of private attribute width to value
+        Args:
+            value: (int)
+        """
         self.__width = value
 
     @property
     def x(self):
+        """
+        Gets the value at private attribute x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        Sets th value of private attribute x to value
+        Args: 
+            value: (int) 
+        """
         self.__x = value
 
     @property
     def y(self):
+        """
+        Gets the value at private attribute y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        Sets the private attribute value to value
+        Args:
+            value: (int)
+        """
         self.__y = value
