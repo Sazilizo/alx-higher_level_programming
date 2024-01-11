757d6a52a010e21f8593a401fde262c6fabe7ab8
root <lizosazi@gmail.com>
2024-01-11 02:30:00 -0800

    refactoring and removing redundant return

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,14 +3,10 @@
 const { argv } = require('node:process');
 
 if (!isNaN(argv[2])) {
-  const loop_limit = Number(argv[2]);
-  if (loop_limit < 0) {
-    return;
-  } else {
-    for (let i = 0; i < loop_limit; i++) {
-      console.log('C is fun');
-    }
+  const loopLimit = Number(argv[2]);
+  for (let i = 0; i < loopLimit; i++) {
+    console.log('C is fun');
   }
 } else {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
